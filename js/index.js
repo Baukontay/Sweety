53edfe6437f8ecc86b794123e5dbd7df09fea31f
Bauka <119521935+Baukontay@users.noreply.github.com>
2022-12-04 00:02:47 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,62 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(43.23517601216518, 76.90991176371539);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 12};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+}
+
+function slowScroll(id) {
+      $('html,body').animate({
+        scrollTop: $(id).offset().top
+      }, 1000);
+    }
+
+    var counter = 1;
+    setInterval (function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 3000);
+
+    $(document).ready(function() {
+var defaults = {
+containerID: 'toTop', // fading element id
+containerHoverID: 'toTopHover', // fading element hover id
+scrollSpeed: 1200,
+easingType: 'linear'
+};
+ 
+$().UItoTop({ easingType: 'easeOutQuart' });
+ 
+});
